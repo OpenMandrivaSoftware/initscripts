f65bb60538c52eeb36780694fad0375bbb432674
Per Øyvind Karlsen (proyvind) <peroyvind@mandriva.org>
2013-03-25 21:59:50 +0100

    remove tmpfiles cleaning of /tmp now that tmpfs is used

--- mandriva.tmpfiles.d
+++ mandriva.tmpfiles.d
@@ -12,22 +12,4 @@ r /etc/killpower
 R /var/lock/cvs/*
 R /var/run/screen/*
 
-r /tmp/.lock.*
-r /tmp/.s.PGSQL.*
-R /tmp/hsperfdata_*
-R /tmp/kde-*
-R /tmp/ksocket-*
-R /tmp/mc-*
-R /tmp/mcop-*
-R /tmp/orbit-*
-R /tmp/ssh-*
-R /tmp/.fam_socket
-R /tmp/.esd
-R /tmp/.esd-*
-R /tmp/pulse-*
-R /tmp/.sawfish-*
-R /tmp/esrv*
-R /tmp/kio*
 R /var/lib/gdm/core.*
-
-R /tmp/gpg-*
